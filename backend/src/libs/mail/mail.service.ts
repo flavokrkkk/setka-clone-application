1230875d6a6d408f748b0d924d02ce064f5aadf4
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-12-16 01:24:01 +0300

    small feat

--- backend/src/libs/mail/mail.service.ts
+++ backend/src/libs/mail/mail.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { render } from "@react-email/components";
 import { ConfirmationTemplate } from "./templates/confirmation.template";
+import { ResetPasswordTemplate } from "./templates/reset-password.template";
 
 @Injectable()
 export class MailService {
@@ -16,6 +17,13 @@ export class MailService {
     const emailHtml = await render(ConfirmationTemplate({ domain, token }));
 
     return this.sendMail(email, "Подтверждение почты", emailHtml);
+  }
+
+  public async sendPasswordResetEmail(email: string, token: string) {
+    const domain = this.configService.getOrThrow<string>("ALLOWED_ORIGIN");
+    const emailHtml = await render(ResetPasswordTemplate({ domain, token }));
+
+    return this.sendMail(email, "Сброс пароля", emailHtml);
   }
 
   private sendMail(email: string, subject: string, body: string) {
---
+++ backend/src/password-recovery/password-recovery.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from "@nestjs/common";
+import { PasswordRecoveryService } from "./password-recovery.service";
+
+@Controller("auth/password-recovery")
+export class PasswordRecoveryController {
+  constructor(private readonly passwordRecoveryService: PasswordRecoveryService) {}
+}
